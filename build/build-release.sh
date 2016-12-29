0c56e5fd06e5446f200d73ed32ea660739f836e0
Dhawal Patel <dhawal55@gmail.com>
2016-12-28 22:40:28 -0800

    Added service-cluster-ip-range as flag to bootkube start

--- build/build-release.sh
+++ build/build-release.sh
@@ -2,8 +2,11 @@
 set -euo pipefail
 
 BOOTKUBE_ROOT=$(git rev-parse --show-toplevel)
-sudo rkt run \
-    --volume bk,kind=host,source=${BOOTKUBE_ROOT} \
-    --mount volume=bk,target=/go/src/github.com/kubernetes-incubator/bootkube \
-    --insecure-options=image docker://golang:1.7.4 --exec /bin/bash -- -c \
-    "cd /go/src/github.com/kubernetes-incubator/bootkube && make release"
+# sudo rkt run \
+#     --volume bk,kind=host,source=${BOOTKUBE_ROOT} \
+#     --mount volume=bk,target=/go/src/github.com/kubernetes-incubator/bootkube \
+#     --insecure-options=image docker://golang:1.7.4 --exec /bin/bash -- -c \
+#     "cd /go/src/github.com/kubernetes-incubator/bootkube && make release"
+docker run \
+  --volume ${BOOTKUBE_ROOT}:/go/src/github.com/kubernetes-incubator/bootkube \
+  golang:1.7.4 /bin/bash -c "cd /go/src/github.com/kubernetes-incubator/bootkube && make release" 
