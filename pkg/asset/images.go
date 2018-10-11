8ac2ef7f67c3f765ca0fdb618dba1ff1a3820da8
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-10-11 14:59:31 +0200

    Bump v1.12.1
    Update Kubernetes dependencies to 1.12.1 and update CoreDNS to 1.2.2

--- pkg/asset/images.go
+++ pkg/asset/images.go
@@ -7,7 +7,7 @@ var DefaultImages = ImageVersions{
 	FlannelCNI:      "quay.io/coreos/flannel-cni:v0.3.0",
 	Calico:          "quay.io/calico/node:v3.0.3",
 	CalicoCNI:       "quay.io/calico/cni:v2.0.0",
-	CoreDNS:         "k8s.gcr.io/coredns:1.1.3",
-	Hyperkube:       "k8s.gcr.io/hyperkube:v1.11.2",
+	CoreDNS:         "k8s.gcr.io/coredns:1.2.2",
+	Hyperkube:       "k8s.gcr.io/hyperkube:v1.12.1",
 	PodCheckpointer: "quay.io/coreos/pod-checkpointer:9dc83e1ab3bc36ca25c9f7c18ddef1b91d4a0558",
 }
